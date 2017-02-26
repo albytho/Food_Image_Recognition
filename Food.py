dd91fd9ed70139e3aeb5a28152604ba62ea7d8ca
Alby Thomas <albytho@umich.edu>
2017-02-25 18:13:48 -0600

    Add files via upload

--- Food.py
+++ Food.py
@@ -1,24 +1,34 @@
 ########### Python 3.2 #############
-import http.client, urllib.request, urllib.parse, urllib.error, base64
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json
 
 headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '',
+    'Ocp-Apim-Subscription-Key': 'bb75187fb0e94731ac28db3ddbc45548',
 }
 
 params = urllib.parse.urlencode({
     # Request parameters
-    'visualFeatures': 'Categories,Description',
+    'visualFeatures': 'Categories,Tags',
     'language': 'en',
 })
 
 try:
     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{'url':'http://bangalorefruits.com/wp-content/uploads/2013/07/orange-02.jpg'}", headers)
+    conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{'url':'https://ylakeland.com/wp-content/uploads/2015/04/blueberry450_221068a_8col.jpg'}", headers)
     response = conn.getresponse()
     data = response.read()
-    print(data)
+    data=data.decode("utf-8")
+    jdata = json.loads(data)
+    potentialFoodList = []
+
+    for x in jdata["tags"]:
+        if(x["confidence"] > 0.90):
+            potentialFoodList.append(x["name"])
+
+    for x in potentialFoodList:
+        print(x)
+        
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
